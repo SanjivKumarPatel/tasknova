80c4ae3365905c27f8c290f165b4ed9b3cabfb77
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-08-24 00:56:36 +0530

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Search, Bell, User, ChevronDown, LogOut } from 'lucide-react'
+import { Search, Bell, User, LogOut } from 'lucide-react'
 import { AuthContext } from '../context/AuthContext'
 
 function Navbar() {
@@ -18,7 +18,7 @@ function Navbar() {
   }
 
   return (
-    <header className='h-[90px] bg-[#050b1a] border-b border-blue-500/10 px-8 flex items-center justify-between'>
+    <header className='h-22 bg-black bg-linear-to-r from-black to to-blue-700 border rounded-md px-8 flex items-center justify-between'>
       {/* search */}
       <div className='relative w-full max-w-xl'>
         <Search
@@ -29,24 +29,17 @@ function Navbar() {
         <input
           type='text'
           placeholder='Search anything...'
-          className='w-full h-14 rounded-2xl border border-blue-500/10 bg-[#0b1328] pl-14 pr-5 text-white placeholder:text-slate-500 outline-none focus:border-blue-500 transition'
+          className='w-full h-14 rounded-2xl border border-blue-500/10 bg-blue-950 pl-14 pr-5 text-white placeholder:text-slate-500 outline-none focus:border-blue-500 transition'
         />
       </div>
 
       {/* right side */}
       <div className='ml-8 flex items-center gap-4'>
-        {/* notifications */}
-        <button onClick={handleNotifications} className='relative flex h-12 w-12 items-center justify-center rounded-2xl border border-blue-500/10 bg-[#0b1328] text-white hover:border-blue-500 transition'>
-          <Bell size={20} />
-
-          <span className='absolute -top-1 -right-1 flex h-5 w-5 items-center justify-center rounded-full bg-blue-600 text-[10px] font-semibold text-white'>
-            3
-          </span>
-        </button>
+        
 
         {/* profile */}
         <div className='flex items-center gap-3 rounded-2xl border border-blue-500/10 bg-[#0b1328] px-4 py-2 min-w-[220px]'>
-          <div className='flex h-11 w-11 items-center justify-center rounded-full bg-gradient-to-br from-blue-500 to-indigo-600 text-white'>
+          <div className='flex h-11 w-11 items-center justify-center rounded-full bg-linear-to-br from-blue-500 to-indigo-600 text-white'>
             <User size={18} />
           </div>
 
@@ -59,9 +52,12 @@ function Navbar() {
               {user?.role || 'Member'}
             </p>
           </div>
-
-          <ChevronDown size={18} className='text-slate-400' />
         </div>
+
+        {/* notifications */}
+        <button onClick={handleNotifications} className='relative flex h-12 w-12 items-center justify-center rounded-2xl border border-blue-500/10 bg-[#0b1328] text-white hover:border-blue-500 transition'>
+          <Bell size={20} />
+        </button>
 
         {/* logout */}
         <button
