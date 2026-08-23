77cf2caa68030f4f6f042cc8f92a3cb07db9a4d6
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-08-24 00:54:18 +0530

    update dashboard layout

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -3,18 +3,20 @@ import Navbar from '../components/Navbar'
 
 function DashboardLayout({ children }) {
   return (
-    <div className='h-screen bg-slate-100 flex overflow-hidden'>
-        {/* sidebar */}
-        <Sidebar />
+    <div className='h-screen bg-gray-100 flex overflow-hidden'>
+      {/* Sidebar */}
+      <Sidebar />
 
-        {/* right section */}
-        <div className='flex-1 flex flex-col overflow-hidden'>
-          {/* Navbar */}
-          <Navbar />
+      {/* Right section */}
+      <div className='flex-1 flex flex-col overflow-hidden'>
+        {/* Navbar */}
+        <Navbar />
 
-          {/* Page content */}
-          <main className='flex-1 p-6 overflow-y-auto'>{children}</main>
-        </div>
+        {/* Page content */}
+        <main className='flex-1 overflow-y-auto'>
+          {children}
+        </main>
+      </div>
     </div>
   )
 }
